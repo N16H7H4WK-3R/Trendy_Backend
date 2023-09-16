6efe523861909c96a41e059c8d852a4fd6bd2847
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-16 14:41:34 +0530

    modified login with unique email

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import status
 from home.models import Login
 from home.serializer import LoginSerializer
 
@@ -23,7 +23,6 @@ def index(request):
 
 @api_view(['GET', 'POST'])
 def login(request):
-
     if request.method == 'GET':
         objs = Login.objects.all()
         serializer = LoginSerializer(objs, many=True)
@@ -31,9 +30,15 @@ def login(request):
 
     elif request.method == 'POST':
         data = request.data
+        # Check for unique Email
+        existing_email = Login.objects.filter(
+            Email=data.get('Email')).first()
+        if existing_email:
+            return Response({'error': 'Email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
         serializer = LoginSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
