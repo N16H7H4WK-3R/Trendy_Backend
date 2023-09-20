e67fbd32840c5a142455920e0b3b78ad1c464048
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-20 23:06:28 +0530

    updates

--- home/views.py
+++ home/views.py
@@ -2,8 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 from home.serializer import UserRegistrationSerializer, UserLoginSerializer, EditProfileSerializer
 
