68e2c2221fbf34eefa4bd0f2f56cf94777695cb9
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-25 20:19:00 +0530

    updates

--- services/models.py
+++ services/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
 
     # Add custom fields here, if needed
 
     def __str__(self):
-        return self.username
+        return f'{self.username} -----> \t {self.id}'
--- services/serializers.py
+++ services/serializers.py
@@ -5,13 +5,19 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password']
+        fields = ['id', 'username', 'email',
+                  'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        first_name = validated_data.get('first_name', '')
+        last_name = validated_data.get('last_name', '')
+
         user = CustomUser(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=first_name,
+            last_name=last_name
         )
         user.set_password(validated_data['password'])
         user.save()
