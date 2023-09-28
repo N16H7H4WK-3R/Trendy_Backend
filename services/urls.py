5fb0ba7b7bc0d0b268a0019f8b40e94c30951801
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-28 23:15:11 +0530

    added fetch_user_data api

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout, edit_profile
+from .views import register_user, user_login, user_logout, edit_profile, fetch_user_data
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('edit-profile/', edit_profile, name='edit_profile'),
+    path('user/', fetch_user_data, name='user_data'),
+    
 ]
--- services/views.py
+++ services/views.py
@@ -74,3 +74,14 @@ def edit_profile(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def fetch_user_data(request):
+    try:
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
