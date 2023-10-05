c1fcfd56a6848b66a7e6954ab51a67da0f652b92
N16H7H4WK-3R <aryan014kumar@gmail.com>
2023-10-05 19:27:23 +0530

    Added required Cart APIs

--- services/urls.py
+++ services/urls.py
@@ -1,14 +1,31 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout, edit_profile, fetch_user_data, fetch_productData, fetch_productDetailData, add_to_cart, fetch_user_cart_data
+from .views import (
+    register_user,
+    user_login,
+    user_logout,
+    edit_profile,
+    fetch_user_data,
+    fetch_productData,
+    fetch_productDetailData,
+    add_to_cart,
+    fetch_user_cart_data,
+    remove_from_cart,
+    update_cart_item_quantity,
+)
+
 
 urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('edit-profile/', edit_profile, name='edit_profile'),
-    path('user/', fetch_user_data, name='user_data'),
-    path('data/', fetch_productData, name='product_data'),
-    path('detail/<str:product_id>/', fetch_productDetailData, name='product_detail_data'),
-    path('cart/', add_to_cart, name='cart'),
-    path('cart-data/', fetch_user_cart_data, name='user_cart_data'),
+    path("register/", register_user, name="register"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("edit-profile/", edit_profile, name="edit_profile"),
+    path("user/", fetch_user_data, name="user_data"),
+    path("data/", fetch_productData, name="product_data"),
+    path(
+        "detail/<str:product_id>/", fetch_productDetailData, name="product_detail_data"
+    ),
+    path("cart/", add_to_cart, name="cart"),
+    path("cart-data/", fetch_user_cart_data, name="user_cart_data"),
+    path("cart-delete/", remove_from_cart, name="remove_from_cart"),
+    path("cart-update/", update_cart_item_quantity, name="update_cart_item_quantity"),
 ]
--- services/views.py
+++ services/views.py
@@ -16,9 +16,9 @@ from .serializers import UserSerializer, CartItemSerializer
 #####################################
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def register_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -26,14 +26,14 @@ def register_user(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def user_login(request):
-    if request.method == 'POST':
-        username = request.data.get('username')
-        password = request.data.get('password')
+    if request.method == "POST":
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         user = None
-        if '@' in username:
+        if "@" in username:
             try:
                 user = CustomUser.objects.get(email=username)
             except ObjectDoesNotExist:
@@ -45,39 +45,46 @@ def user_login(request):
         if user:
             serializer = UserSerializer(user)
             token, _ = Token.objects.get_or_create(user=user)
-            data = {'token': token.key, 'user': serializer.data}
+            data = {"token": token.key, "user": serializer.data}
             return Response(data, status=status.HTTP_200_OK)
 
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+        )
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def user_logout(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             request.user.auth_token.delete()
-            return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+            return Response(
+                {"message": "Successfully logged out."}, status=status.HTTP_200_OK
+            )
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 
-@api_view(['PUT'])
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def edit_profile(request):
     try:
         user = request.user
         serializer = UserSerializer(
-            user, data=request.data, partial=True)  # Allowing partial updates
+            user, data=request.data, partial=True
+        )  # Allowing partial updates
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def fetch_user_data(request):
     try:
@@ -85,7 +92,7 @@ def fetch_user_data(request):
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 #####################################
@@ -93,30 +100,37 @@ def fetch_user_data(request):
 #####################################
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def fetch_productData(request):
     try:
-        with open('/home/aryangupta/Personal_Space/Trendy_Backend/media/productDetailData.json', 'r') as json_file:
+        with open(
+            "/home/aryangupta/Personal_Space/Trendy_Backend/media/productDetailData.json",
+            "r",
+        ) as json_file:
             data = json.load(json_file)
         return Response(data, status=status.HTTP_200_OK)
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def fetch_productDetailData(request, product_id):
     try:
-        with open('/home/aryangupta/Personal_Space/Trendy_Backend/media/productDetailData.json', 'r') as json_file:
+        with open(
+            "/home/aryangupta/Personal_Space/Trendy_Backend/media/productDetailData.json",
+            "r",
+        ) as json_file:
             data = json.load(json_file)
-        product = next(
-            (item for item in data if item["productId"] == product_id), None)
+        product = next((item for item in data if item["productId"] == product_id), None)
 
         if product:
             return Response(product, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND
+            )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 #####################################
@@ -124,36 +138,99 @@ def fetch_productDetailData(request, product_id):
 #####################################
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def add_to_cart(request):
     user = request.user
     serializer = CartItemSerializer(data=request.data)
 
     if serializer.is_valid():
-        product_id = serializer.validated_data['product_id']
-        product_price = serializer.validated_data['product_price']
-        product_image_url = serializer.validated_data['product_image_url']
-        quantity = serializer.validated_data['quantity']
+        product_id = serializer.validated_data["product_id"]
+        product_price = serializer.validated_data["product_price"]
+        product_image_url = serializer.validated_data["product_image_url"]
+        quantity = serializer.validated_data["quantity"]
 
         # Add item to the cart using the CartItem model method
-        CartItem.add_to_cart(user, product_id, quantity, product_price,
-                             product_image_url)
+        CartItem.add_to_cart(
+            user, product_id, quantity, product_price, product_image_url
+        )
 
-        return Response({'message': 'Item added to cart successfully'}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"message": "Item added to cart successfully"},
+            status=status.HTTP_201_CREATED,
+        )
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def fetch_user_cart_data(request):
     try:
         user = request.user
         cart_items = CartItem.objects.filter(user=user)
-        serializer = CartItemSerializer(
-            cart_items, many=True)
+        serializer = CartItemSerializer(cart_items, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def remove_from_cart(request):
+    try:
+        user = request.user
+        product_id = request.data.get("product_id")
+
+        cart_item = CartItem.objects.filter(user=user, product_id=product_id).first()
+
+        if cart_item:
+            cart_item.delete()
+            return Response(
+                {"message": "Item removed from cart successfully"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        else:
+            return Response(
+                {"message": "Item not found in the cart"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def update_cart_item_quantity(request):
+    try:
+        user = request.user
+        product_id = request.data.get("product_id")
+        new_quantity = int(
+            request.data.get("quantity")
+        )
+
+        # Check for the item to exists in the user's cart
+        cart_item = CartItem.objects.filter(user=user, product_id=product_id).first()
+
+        if cart_item:
+            if new_quantity > 0:
+                cart_item.quantity = new_quantity
+                cart_item.save()  # Update the cart item quantity
+                serializer = CartItemSerializer(cart_item)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                cart_item.delete()  # delete the cart item
+                return Response(
+                    {"message": "Item removed from cart successfully"},
+                    status=status.HTTP_204_NO_CONTENT,
+                )
+        else:
+            return Response(
+                {"message": "Item not found in the cart"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
